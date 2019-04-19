480e82dadf74679af39c6273b370517738da82c9
sfanpro <st.sergeyf@gmail.com>
2019-04-19 14:37:03 +0300

    read method arguments with JoinPoints

--- spring-aop/src/main/java/com/goodteam/aop/aspect/MyDemoLoggingAspect.java
+++ spring-aop/src/main/java/com/goodteam/aop/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,9 @@
 package com.goodteam.aop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -16,27 +17,33 @@ public class MyDemoLoggingAspect {
 	// start with @Befor advice
 
 	// declare pointcut declaration
-	@Pointcut("execution(* com.goodteam.aop.dao.*.*(..))")
-	private void forDaoPackage() {
-	}
-
-	// create pointcut for getter method
-	@Pointcut("execution(* com.goodteam.aop.dao.*.get*(..))")
-	private void getter() {
-	}
-
-	// create pointcut for setter method
-	@Pointcut("execution(* com.goodteam.aop.dao.*.set*(..))")
-	private void setter() {
-	}
-
-	// create point: include package... exclude getter/setter methods
-	@Pointcut("forDaoPackage() && !(getter() || setter())")
-	private void forDaoPackageNoGetterSetter() {
-	}
+//	@Pointcut("execution(* com.goodteam.aop.dao.*.*(..))")
+//	private void forDaoPackage() {
+//	}
+//
+//	// create pointcut for getter method
+//	@Pointcut("execution(* com.goodteam.aop.dao.*.get*(..))")
+//	private void getter() {
+//	}
+//
+//	// create pointcut for setter method
+//	@Pointcut("execution(* com.goodteam.aop.dao.*.set*(..))")
+//	private void setter() {
+//	}
+//
+//	// create point: include package... exclude getter/setter methods
+//	@Pointcut("forDaoPackage() && !(getter() || setter())")
+//	private void forDaoPackageNoGetterSetter() {
+//	}
 
 	@Before("com.goodteam.aop.aspect.goodTeamAopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforAddAccountAdvice() {
+	public void beforAddAccountAdvice(JoinPoint theJoinPoint) {
 		System.out.println("\n======>>> Executing @Before advice on addAccount()");
+		// display the method signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSig);
+
+		// display method argument
 	}
+
 }
